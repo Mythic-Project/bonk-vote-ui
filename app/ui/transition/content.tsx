54ace35181a969c09639b4a9c76ae41013f18e43
Utkarsh <83659045+0xShuk@users.noreply.github.com>
2024-11-08 02:41:11 +0530

    remove token deposit in the popup

--- app/ui/transition/content.tsx
+++ app/ui/transition/content.tsx
@@ -17,7 +17,6 @@ function TransitionContent({closeModal} : {closeModal: (b: boolean) => void}) {
   const voteRecords = useGetVoteRecords(realmMeta.name).data
   const tokenOwnerRecord = useGetTokenOwnerRecord(realmMeta.name).data
   const tokenHolding = useGetTokensHolding(realmMeta.name).data
-  const daoMintInfo = useGetDaoMintData(realmMeta.name).data
 
   const [errorMsg, setError] = useState("")
   const [depositTokens, setDepositTokens] = useState(false)
@@ -55,15 +54,6 @@ function TransitionContent({closeModal} : {closeModal: (b: boolean) => void}) {
       <div className="text-secondary-text text-[16px] flex flex-col items-center text-center">
         <p className="">It appears that your voting profile configuration is incomplete</p>
         <p className="">Click the button below to start participating in the DAO</p>
-        {tokenHolding?.length && new BN(tokenHolding[0].balance).gt(new BN(0)) && daoMintInfo ?
-          <div className="flex gap-1 mt-4">
-            <input type="checkbox" className="w-4" onChange={e => setDepositTokens(e.target.checked)}/>
-            <p>Deposit </p>
-            <FormatBalance decimals={daoMintInfo[0].decimals} weight={new BN(tokenHolding[0].balance) }/>
-            {daoMintInfo[0].name ?? "Tokens"}
-          </div> : 
-          ""
-        }
       </div>
       <ActiveButton 
         title={transitionTokensPending ? "Updating..." : "Completing Voting Profile"}
